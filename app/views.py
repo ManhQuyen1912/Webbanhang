39cdc81425b60af37ea6b258648821a4dd7a7cb0
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-12-08 10:36:31 +0700

    add cart and checkout page

--- app/views.py
+++ app/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(req):
-    return render(req,'app/home.html')
+    context={}
+    return render(req,'app/home.html',context)
+def cart(req):
+    context={}
+    return render(req,'app/cart.html',context)
+def checkout(req):
+    context={}
+    return render(req,'app/checkout.html',context)
