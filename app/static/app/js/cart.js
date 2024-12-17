9a171f64a1c7ce85c42d9f57e8cd8f36697e7c52
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-12-17 09:05:01 +0700

    link add to cart button to models order

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -8,7 +8,23 @@ for (i=0; i<updateBtns.length;i++){
         if (user === "AnonymousUser"){
             console.log('user not logged in')
         } else {
-            console.log('user logged in, success add')
+            updateUserOrder(productId,action)
         }
+    })
+}
+
+function updateUserOrder(productId,action){
+    var url = '/update_item/'
+    fetch(url,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId':productId,'action':action})
+    }).then((response) =>{
+        return response.json()
+    }).then((data)=>{
+        console.log(data)
     })
 }
