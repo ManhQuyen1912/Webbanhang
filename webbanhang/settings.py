2b5627d26241f0f392847ebe5b6abce73bd719ec
ManhQuyen1912 <nmquyen1912@gmail.com>
2025-01-20 11:43:38 +0700

    fix deployment

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c@s75*&g8sof1$gl)a)l=g-#2vt3+q65x#%qt5@(@z5hgv2n8t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["vnsalestore.onrender.com","127.0.0.1"]
 
